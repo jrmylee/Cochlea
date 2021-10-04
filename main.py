c513d67f2db6df13a0309800fe09ac089fe40250
jrmylee <jrmylee@berkeley.edu>
2021-10-04 13:14:25 -0700

    various changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cochlea import generate_spectrograms_from_ds
 from augmentations.naive import random_augmentations
 from transformations.melspectrogram import mel_spec_from_audio
+from transformation.stft import stft_stacked
 
 with open("params.json") as file:
     hparams = json.load(file, object_hook=lambda d: SimpleNamespace(**d)
@@ -8,7 +9,7 @@ with open("params.json") as file:
 generate_spectrograms_from_ds(
     ds_path="/home/jerms/data/maestro-v3.0.0", 
     mapping_filename="maestro-v3.0.0.csv", 
-    save_path="/home/jerms/disk1/spectrograms/aug", 
-    transform_fn=mel_spec_from_audio,
-    augment_fn=random_augmentations,
+    save_path="/home/jerms/disk1/spectrograms/stft", 
+    transform_fn=stft_stacked,
+    augment_fn=None,
     hparams=hparams)
--- transformations/stft.py
+++ transformations/stft.py
@@ -14,6 +14,14 @@ def istft(y, hparams):
         y, hop_length=hparams.hop_length, win_length=hparams.win_length
     )
 
+# returns a stft with the real and imag components stacked
+# shape of (n_fft / 2 + 1, t, 2)
+def stft_stacked(y, hparams):
+    transformed = stft(y, hparams)
+    real, imag = transformed.real, transformed.imag
+    stacked = np.stack((real, imag), axis=-1)
+    return stacked
+
 # use this one when istft is fixed!
 def istft_tf(stfts, hparams):
     return tf.signal.inverse_stft(
